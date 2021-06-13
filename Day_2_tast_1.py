5a78ea0d3d1474b6c9d0e1e13495c670680103c6
urvirath0d <74901176+urvirath0d@users.noreply.github.com>
2021-06-13 23:18:08 +0530

    Add files via upload
    #30 days 30 hours
    #Python-Bootcamp
    #Python-Internship

---
+++ Day_2_tast_1.py
@@ -0,0 +1,48 @@
+#Day-2
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+Hours = "thirty"
+print(Hours)
+#INDEX USING STRINGS
+Days = "Thirty days"
+print(Days[0])
+print(Days[7:])
+print(Days[4:8])
+print(Days[:-3])
+#How to print the particular character from certain text?
+Challenge = "I will win"
+print(Challenge[7:10])
+print(Challenge[4:])
+print(Challenge[::-5])
+print(Challenge[1:5])
+print(Challenge[4:5])
+Challenge = "I will win"
+print(len(Challenge))
+#Convert String into lower character;
+Challenge = "I will win"
+print(Challenge.lower())
+#String Concatention-Joining two strings
+a = "30 Days"
+b = "30 hours"
+c = a+b
+print(c)
+a = "30 Days"
+b = "30 hours"
+c = a+" "+b
+print(c)
+#casefold() - usage
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.capitalize()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.find('and')
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.isalpha()
+print(x)
+text = "Thirty days and Thirty hours"
+x = text.isalnum()
+print(x)
