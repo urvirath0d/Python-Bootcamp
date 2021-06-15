1bc5256dc218334e8f60c7e27acb019021bcc987
urvirath0d <74901176+urvirath0d@users.noreply.github.com>
2021-06-15 16:23:55 +0530

    Add files via upload
    Day-4

---
+++ Day_4.py
@@ -0,0 +1,19 @@
+print("Task-1")
+a=b=c=5
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("New Value Of a is:", a/10)
+print("New Value Of b is:", b*50)
+print("New Value Of c is:", c+60)
+print("Task-2")
+U="Class"
+print(U)
+print("After Changing 3rd Variable To G:", U.replace('a','G'))
+print("Task-3")
+x = 1.2
+y = 4
+print("x:", x)
+print("x from float to int is:",int(x))
+print("y:", y)
+print("y from int to float is:",float(y))
