375a4b51e0c678c3d59e3c38f011c36ce5bb9401
urvirath0d <74901176+urvirath0d@users.noreply.github.com>
2021-06-16 21:46:10 +0530

    Add files via upload
    Day-5

---
+++ Day_5.py
@@ -0,0 +1,26 @@
+#Task1
+#created a list
+A = [1, 2, 5]
+#Added an item
+A.append(6)
+print(A)
+#deleted a item
+del A[:1]
+print(A)
+#max & min number of list stored in variables
+b = max(A)
+c = min(A)
+print(b)
+print(c)
+#Task2
+#created a tuple
+t = (7, 'u', 9, 'k')
+print(t)
+#revesed that tuple
+print('After reversing the tuple:', t[::-1])
+#Task3
+#converted tuple into list
+print('Type of t is:',type(t))
+l=list(t)
+print('Type of l is:',type(l))
+print(l)
